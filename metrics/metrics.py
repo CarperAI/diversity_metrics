68add9d70a4cf178405024dc48b47059df55bb78
Ryan Teehan <rsteehan@gmail.com>
2023-07-05 21:51:26 -0400

    add simple embed funct

--- metrics/metrics.py
+++ metrics/metrics.py
@@ -1,5 +1,5 @@
 from nltk.translate.bleu_score import sentence_bleu
-
+import torch
 
 def self_bleu(sentences):
     '''
@@ -17,3 +17,14 @@ def self_bleu(sentences):
         scores.append(sentence_bleu(references, hypothesis_split))
 
     return sum(scores) / len(scores)
+
+def emb_sim(ref_model, tokenizer, s1, s2):
+    pass
+
+def embed(ref_model, tokenizer, sentence):
+    inputs = tokenizer(sentence, return_tensors = 'pt')
+    with torch.no_grad():
+        outputs = ref_model(inputs**, output_hidden_states = True)
+
+    return outputs.hidden_states[-1]
+
